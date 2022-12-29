26272da4f8ea1c96ca3f95258af075e00fa0d0d5
Şeyma Çatıkkaşlı <seymactk1@gmail.com>
2022-12-29 11:17:18 +0300

    layout_builder_yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_top_widget/widgets/layout_builder.dart';
 import 'package:flutter_top_widget/widgets/limited_box.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LimitedBoxWidget(),
+      home: const LayoutBuilderWidget(),
     );
   }
 }
