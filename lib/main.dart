40252c346113080733173214779c5321c3bbd14d
Şeyma Çatıkkaşlı <seymactk1@gmail.com>
2022-12-29 11:09:32 +0300

    limited_box_yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_top_widget/widgets/linear_progress_indicator.dart';
+import 'package:flutter_top_widget/widgets/limited_box.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LinearProgressWidget(),
+      home: const LimitedBoxWidget(),
     );
   }
 }
