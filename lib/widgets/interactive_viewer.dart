dfc66a17c1d7204a9ebd65c6d17a8b53f17de8b4
Şeyma Çatıkkaşlı <seymactk1@gmail.com>
2022-12-29 11:23:42 +0300

    interactive_viewer_yapıldı

---
+++ lib/widgets/interactive_viewer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class InteractiveViewerWidget extends StatefulWidget {
+  const InteractiveViewerWidget({super.key});
+
+  @override
+  State<InteractiveViewerWidget> createState() =>
+      _InteractiveViewerWidgetState();
+}
+
+class _InteractiveViewerWidgetState extends State<InteractiveViewerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orangeAccent,
+      body: InteractiveViewer(
+        boundaryMargin: const EdgeInsets.all(double.infinity),
+        child: Scaffold(appBar: AppBar(
+          title: const Text('Flutter Map'),
+        ),),
+      ),
+    );
+  }
+}
